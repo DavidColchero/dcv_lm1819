f9082fed07c3fd4b43d048b078bffa496d0d5a84
Alumno <davidcolchero@hotmail.es>
2019-01-28 13:58:24 +0100

    config

--- PHP/info.php
+++ PHP/info.php
@@ -6,3 +6,14 @@
     <?php phpinfo(); ?>
 </body>
 </html>
+
+<!-- 
+<IfModule alias_module>
+Alias /curso "C:\Users\Alumno\Documents\REPOSITORIO\dcv_lm1819\PHP"
+<Directory "C:\Users\Alumno\Documents\REPOSITORIO\dcv_lm1819\PHP">
+AllowOverride All
+Options None
+Require all granted
+</Directory>
+</IfModule>
+-->
