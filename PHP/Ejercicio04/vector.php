30ee530d5a56c31e57bbadfa5490a3120853df5e
Alumno <davidcolchero@hotmail.es>
2019-02-11 14:14:57 +0100

    11/02/2019

---
+++ PHP/Ejercicio04/vector.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Vector</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+</head>
+<body>
+
+    <?php 
+        $vector = array(6, 8, 3, 2, 13, 17);
+        sort($vector);
+        print $vector[5] . " ";
+        print $vector[4] . " ";
+        print $vector[3] . " ";
+    ?>
+    
+</body>
+</html>
