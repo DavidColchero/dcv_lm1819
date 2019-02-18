806bebbfcd3c528cbf263b51fd88f308920e5785
DavidColchero <43633494+DavidColchero@users.noreply.github.com>
2019-02-18 14:42:53 +0100

    Add files via upload

--- PHP/Ejercicio06/resultado.php
+++ PHP/Ejercicio06/resultado.php
@@ -1,11 +1,15 @@
 <?php
-    if ($_REQUEST["nombre" != ""]) {
-    print "<p>Su nombre es $_REQUEST[nombre]</p>"
+    $nombre = trim(htmlspecialchars(strip_tags($_REQUEST["nombre"]), ENT_QUOTES, "UTF-8"));
+    if ($nombre != "") {
+    print "<p>Su nombre es $nombre</p>";
+    }
+    if (isset($_REQUEST["opcion"])) {
+        print "<p>Opción marcada</p>";
     }
 
-    if (isset($_REQUEST["opcion"])) {
-        print
-    }
+
     print_r($_REQUEST);
     print_r($_FILES);
+    //Formulario que pida la edad y si ha puesto un numero nos diga el numero que hemos puesto y si no es un numero que de error.
 ?>
+
