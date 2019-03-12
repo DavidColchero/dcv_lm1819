f66d89d23144cb98c9192019832c7853f4a9dad3
unknown <davidcolchero@hotmail.es>
2019-03-12 16:12:04 +0100

    No Base de Datos

--- Base de Datos/HR_Subconsultas.sql
+++
@@ -1,4 +0,0 @@
--- Saber el nombre, el trabajo y el salario de los empleados que cobran mas de la media 
-
-SELECT first_name, last_name, salary FROM EMPLOYEES
-    WHERE salary > (SELECT AVG(salary) FROM EMPLOYEES);
