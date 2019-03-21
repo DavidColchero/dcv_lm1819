8cc4847519174c010a23174b1261841020481dff
Alumno <davidcolchero@hotmail.es>
2019-03-21 14:37:08 +0100

    CSS

--- PHP/Ejercicio21/principal.php
+++ PHP/Ejercicio21/principal.php
@@ -1,23 +1,36 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Principal</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-    <form action="datos.php" method="POST">
-            <h2>Email</h2>
-            <p>
-                <label for="email">Email: </label>
-                <input type="email" name="email" id="email">
-            </p>
+<html lang="es">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1,
+            shrink-to-fit=no">
 
-            <p>
-                <input type="submit" value="Buscar">
-            </p>
-    </form>
-    
-</body>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
+
+        <title>Halopedia</title>
+    </head>
+    <body>
+        <div class="container">
+            <form action="datos.php" method="POST">
+                <h2>Email</h2>
+                <p>
+                    <label for="email">Email: </label>
+                    <input type="email" name="email" id="email">
+                </p>
+
+                <p>
+                    <input type="submit" value="Buscar">
+                </p>
+            </form>
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.3.1.slim.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
