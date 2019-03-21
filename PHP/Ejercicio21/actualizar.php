4e0fc7fbc0fc59ebad9a0d861a3973a439db917f
unknown <davidcolchero@hotmail.es>
2019-03-21 17:29:21 +0100

    DFVDFV

--- PHP/Ejercicio21/actualizar.php
+++ PHP/Ejercicio21/actualizar.php
@@ -1,26 +1,45 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Actualizar</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-    <?php
-        $identificador = trim(htmlspecialchars($_REQUEST["identificador"], ENT_QUOTES, "UTF-8"));
-        $nombre = trim(htmlspecialchars($_REQUEST["nombre"], ENT_QUOTES, "UTF-8"));
-        $curso = trim(htmlspecialchars($_REQUEST["curso"], ENT_QUOTES, "UTF-8"));
+<html lang="es">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1,
+            shrink-to-fit=no">
 
-        $conexion = mysqli_connect("localhost", "lm_1819", "Kilobyte1", "cursophp")
-            or die("Problemas en la conexion");
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
 
-        $registros = mysqli_query($conexion, "UPDATE ALUMNOS SET nombre = '$nombre',
-            codigocurso = $curso WHERE idAlumno = $identificador")
-            or die("Problemas en la actualización:".mysqli_error($conexion));
-        
-        echo "<p>Alumno Actualizado</p>"
-    ?>
-    
-</body>
+        <title>Actualizar</title>
+    </head>
+    <body>
+        <div class="container">
+            <?php
+                $identificador = trim(htmlspecialchars($_REQUEST["identificador"], ENT_QUOTES, "UTF-8"));
+                $nombre = trim(htmlspecialchars($_REQUEST["nombre"], ENT_QUOTES, "UTF-8"));
+                $curso = trim(htmlspecialchars($_REQUEST["curso"], ENT_QUOTES, "UTF-8"));
+
+                $conexion = mysqli_connect("localhost", "lm_1819", "Kilobyte1", "cursophp")
+                    or die("Problemas en la conexion");
+
+                $registros = mysqli_query($conexion, "UPDATE ALUMNOS SET nombre = '$nombre',
+                    codigocurso = $curso WHERE idAlumno = $identificador")
+                    or die("Problemas en la actualización:".mysqli_error($conexion));
+                
+                print "<div class='alert alert-primary' role='alert'>
+                            Alumno actualizado!
+                        </div>";
+        ?>
+
+    <form action="principal.php">
+        <input class="btn btn-primary" type="submit" value="Volver">
+    </form> 
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.3.1.slim.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
